13657d3da57a45e34c40e89f09e8fa9a90b7a8ce
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-03-25 02:31:49 -0600

    refactorizacion de comando clear

--- commands/clear.js
+++ commands/clear.js
@@ -17,35 +17,26 @@ module.exports = {
         const user = interaction.options.getUser('user');
         const amount = interaction.options.getInteger('amount');
 
-        if (interaction.member.roles.cache.some(role => role.name === 'mod')) {
-            if (amount < 1 || amount > 100) {
-                return interaction.reply({ content: 'ingresa un numero de entre 1 y 100 mensajes', ephemeral: true });
-            }
-
-            if (user) {
-                interaction.channel.messages.fetch({
-                    limit: 100
-                }).then((messages) => {
-                    const userMessages = [];
-                    messages.filter(m => m.author.id === user.id).forEach(msg => {
-                        userMessages.push(msg);
-                        if (userMessages.length === amount) {
-                            interaction.channel.bulkDelete(userMessages).then(() => {
-                                return interaction.reply({ content: `se borro ${amount} ${amount === 1 ? 'mensaje' : 'mensajes'} de ${user.username.toLowerCase()}`, ephemeral: true });
-                            });
-                        }
-                    })
-                })
-            } else {
-                await interaction.channel.bulkDelete(amount, true).catch(error => {
-                    console.error(error);
-                    interaction.reply({ content: 'hubo un error al eliminar mensajes en el servidor!', ephemeral: true });
+        if (amount < 1 || amount > 100) return interaction.reply({ content: 'ingresa un numero de entre 1 y 100 mensajes', ephemeral: true });
+    
+        if (!interaction.member.roles.cache.some(role => role.name === 'mod')) return interaction.reply({content: 'no tienes permisos para ejecutar este comando', ephemeral: true});
+        else if (user) {
+            interaction.channel.messages.fetch({
+                limit: 100
+            }).then((messages) => {
+                const userMessages = [];
+                messages.filter(m => m.author.id === user.id).forEach(msg => {
+                    userMessages.push(msg);
+                    if (userMessages.length === amount) return interaction.channel.bulkDelete(userMessages).then(() => interaction.reply({ content: `se borro \`${amount}\` ${amount === 1 ? 'mensaje' : 'mensajes'} de \`${user.username}\``, ephemeral: true }));
                 });
-                
-                interaction.reply({content: 'mensajes eliminados', ephemeral: true});
-            }
+            });
         } else {
-            interaction.reply({content: 'no tienes permisos para ejecutar este comando', ephemeral: true});
+            await interaction.channel.bulkDelete(amount, true).catch(error => {
+                console.error(error);
+                return interaction.reply({ content: 'hubo un error al eliminar mensajes en el servidor!', ephemeral: true });
+            });
+            
+            return interaction.reply({content: 'mensajes eliminados', ephemeral: true});
         }
     },
 };
